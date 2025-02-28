98656ea57247ee98d9c84c315cb8a852713fd1e3
Todd Goodall <10576633+goodallt@users.noreply.github.com>
2025-02-28 15:06:18 -0800

    create script for generating fake loads data

---
+++ generateLoads.js
@@ -0,0 +1,40 @@
+import { faker } from "@faker-js/faker";
+
+function createStop() {
+  const startAt = faker.date.soon();
+
+  return {
+    id: faker.string.uuid(),
+    name: faker.location.city(),
+    address: faker.location.streetAddress(),
+    city: faker.location.city(),
+    state: faker.location.state(),
+    zip: faker.location.zipCode(),
+    startAt,
+    endAt: faker.date.soon({ refDate: startAt }),
+  };
+}
+
+function createLoad() {
+  return {
+    id: faker.string.uuid(),
+    number: faker.string.alphanumeric(6),
+    customer: {
+      id: faker.string.uuid(),
+      name: faker.company.name(),
+    },
+    driver: {
+      id: faker.string.uuid(),
+      name: faker.person.fullName(),
+    },
+    truck: {
+      id: faker.string.uuid(),
+      number: faker.string.alphanumeric(4),
+    },
+    stops: [createStop(), createStop()],
+  };
+}
+
+const loads = Array.from({ length: 10 }, createLoad);
+
+console.log(JSON.stringify(loads, null, 2));
