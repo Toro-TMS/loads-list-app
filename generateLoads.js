f1659bd6360bd5c8e7c4a21fca56ddfe57787420
Todd Goodall <10576633+goodallt@users.noreply.github.com>
2025-03-04 13:30:13 -0800

    extract createLoad function into file

--- generateLoads.js
+++ generateLoads.js
@@ -1,39 +1,4 @@
-import { faker } from "@faker-js/faker";
-
-function createStop() {
-  const startAt = faker.date.soon();
-
-  return {
-    id: faker.string.uuid(),
-    name: faker.location.city(),
-    address: faker.location.streetAddress(),
-    city: faker.location.city(),
-    state: faker.location.state(),
-    zip: faker.location.zipCode(),
-    startAt,
-    endAt: faker.date.soon({ refDate: startAt }),
-  };
-}
-
-function createLoad() {
-  return {
-    id: faker.string.uuid(),
-    number: faker.string.alphanumeric({ casing: 'upper', length: 6 }),
-    customer: {
-      id: faker.string.uuid(),
-      name: faker.company.name(),
-    },
-    driver: {
-      id: faker.string.uuid(),
-      name: faker.person.fullName(),
-    },
-    truck: {
-      id: faker.string.uuid(),
-      number: faker.string.numeric(4),
-    },
-    stops: [createStop(), createStop()],
-  };
-}
+import createLoad from "./src/createLoad";
 
 const loads = Array.from({ length: 10 }, createLoad);
 
---
+++ src/createLoad.js
@@ -0,0 +1,38 @@
+import { faker } from "@faker-js/faker";
+
+function createStop() {
+  const startAt = faker.date.soon();
+
+  return {
+    id: faker.string.uuid(),
+    name: faker.location.city(),
+    address: faker.location.streetAddress(),
+    city: faker.location.city(),
+    state: faker.location.state(),
+    zip: faker.location.zipCode(),
+    startAt,
+    endAt: faker.date.soon({ refDate: startAt }),
+  };
+}
+
+function createLoad() {
+  return {
+    id: faker.string.uuid(),
+    number: faker.string.alphanumeric({ casing: 'upper', length: 6 }),
+    customer: {
+      id: faker.string.uuid(),
+      name: faker.company.name(),
+    },
+    driver: {
+      id: faker.string.uuid(),
+      name: faker.person.fullName(),
+    },
+    truck: {
+      id: faker.string.uuid(),
+      number: faker.string.numeric(4),
+    },
+    stops: [createStop(), createStop()],
+  };
+}
+
+export default createLoad;
