dfe5caeb43962fe97287465c2a1f696f90d72454
Todd Goodall <10576633+goodallt@users.noreply.github.com>
2025-03-04 13:30:40 -0800

    create one example load as starting point instead of empty array

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import Load from "./Load"
 import "./App.css"
+import createLoad from "./createLoad"
+
+const exampleLoad = createLoad();
 
 function App() {
-  const loads = [];
-
   return (
     <div className="p-10">
       <h1 className="text-2xl">Loads</h1>
       <div className="space-y-4 mt-4">
-        {loads.map((load) => (
-          <Load key={load.id} load={load} />
-        ))}
+        <Load load={exampleLoad} />
       </div>
     </div>
   )
